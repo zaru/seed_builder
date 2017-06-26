acf0efcc1938455876bd96c31452d173405eb7d1
Kotaro Mochizuki <mochizuki@basicinc.jp>
2017-06-26 15:27:06 +0900

    Replace class_method with instance_method

--- lib/seed_builder/validate/length.rb
+++ lib/seed_builder/validate/length.rb
@@ -1,14 +1,18 @@
 module SeedBuilder
   module Validate
     class Length < SeedBuilder::Validate::Base
-      class << self
-        def call data, entity
-          # TODO: String型しか想定していないので他のも必要であれば対応する
-          return data.ljust @maximum, "a" if data.size < @minimum
-          return data[0, @maximum - 1] if data.size > @maximum
-          data
-        end
+
+      def initialize maximum:, minimum:
+        @maximum = maximum
+        @minimum = minimum
       end
+
+      # TODO: String型しか想定していないので他のも必要であれば対応する
+      def call data, entity
+        return data.ljust @maximum, "a" if data.size < @minimum
+        return data[0, @maximum - 1]    if data.size > @maximum
+        data
+      end
+
     end
   end
-end
