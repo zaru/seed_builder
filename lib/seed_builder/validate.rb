3249329eda9f9d4c08906be2318b1d96b2808689
Kotaro Mochizuki <mochizuki@basicinc.jp>
2017-06-28 16:25:11 +0900

    Fix require...

--- lib/seed_builder/validate.rb
+++ lib/seed_builder/validate.rb
@@ -1,5 +1,4 @@
 require "seed_builder/validate/base"
-require "seed_builder/validate/unique/string"
 
 require "seed_builder/validate/length_validator"
 require "seed_builder/validate/numericality_validator"
